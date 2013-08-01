cf6d136e541227a147ad9a903a3dc9791fe9bbd4
Amos Wenger <amos@lockfree.ch>
2013-08-02 01:31:41 +0200

    Initial hardware detection support for Windows

---
+++ Library/Homebrew/os/windows/hardware.rb
@@ -0,0 +1,37 @@
+module WindowsCPUs
+  OPTIMIZATION_FLAGS = {}.freeze
+  def optimization_flags; OPTIMIZATION_FLAGS; end
+
+  def type
+    @cpu_type ||= case wmic('Manufacturer')
+      when /GenuineIntel/
+        :intel
+      else
+        :dunno
+      end
+  end
+
+  def family
+    :dunno
+  end
+  alias_method :intel_family, :family
+
+  def cores
+    wmic('NumberOfLogicalProcessors').to_i
+  end
+
+  def bits
+    is_64_bit? ? 64 : 32
+  end
+
+  def is_64_bit?
+    return @is_64_bit if defined? @is_64_bit
+    @is_64_bit = /64/ === wmic('AddressWidth')
+  end
+
+  private
+
+  def wmic(property)
+    `wmic cpu get #{property}`.split("\n")[2]
+  end
+end
