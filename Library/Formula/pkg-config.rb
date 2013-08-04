1db8275dff779d967ad57bc04288499a0a3b6d0a
Amos Wenger <amos@lockfree.ch>
2013-08-04 16:03:42 +0200

    Add explicit version for pkg-config

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -4,6 +4,7 @@ class PkgConfig < Formula
   homepage 'http://sourceforge.net/projects/pkgconfiglite/'
   url 'http://downloads.sourceforge.net/project/pkgconfiglite/0.27.1-1/pkg-config-lite-0.27.1-1.tar.gz'
   sha256 '0fcac60ae7ad8e705c30fb5f8f83e75b09f54e1c80a2f6814c346bf5f1dba9a0'
+  version '0.27.1-1'
 
   def install
     paths = %W[
