a3223e9f557a7ffca7cfb2151503075937e5dbaf
Amos Wenger <amos@lockfree.ch>
2013-08-04 18:07:18 +0200

    openssl - not keg-only, use mingw options, not darwin

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -6,21 +6,19 @@ class Openssl < Formula
   mirror 'http://mirrors.ibiblio.org/openssl/source/openssl-1.0.1e.tar.gz'
   sha256 'f74f15e8c8ff11aa3d5bb5f276d202ec18d7246e95f961db76054199c69c1ae3'
 
-  keg_only :provided_by_osx,
-    "The OpenSSL provided by OS X is too old for some software."
-
   def install
     args = %W[./Configure
                --prefix=#{prefix}
                --openssldir=#{openssldir}
                zlib-dynamic
                shared
+               no-asm
              ]
 
     if MacOS.prefer_64_bit?
-      args << "darwin64-x86_64-cc" << "enable-ec_nistp_64_gcc_128"
+      args << "mingw64"
     else
-      args << "darwin-i386-cc"
+      args << "mingw"
     end
 
     system "perl", *args
