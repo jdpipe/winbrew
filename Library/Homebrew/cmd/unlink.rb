ef88120494e7656bc788d40185f5598fd814f8fd
Amos Wenger <amos@lockfree.ch>
2013-08-04 16:30:52 +0200

    Proper warning when trying to unlink a non-linked keg

--- Library/Homebrew/cmd/unlink.rb
+++ Library/Homebrew/cmd/unlink.rb
@@ -4,6 +4,12 @@ module Homebrew extend self
 
     ARGV.kegs.each do |keg|
       keg.lock do
+        unless keg.linked?
+          opoo "Not linked: #{keg}"
+          puts "To link: brew link #{keg.fname}"
+          next
+        end
+
         print "Unlinking #{keg}... "
         puts "#{keg.unlink} links removed"
       end
