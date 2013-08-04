a337506e590c4316cd38dc6369475c33ec84d3af
Amos Wenger <amos@lockfree.ch>
2013-08-04 15:25:30 +0200

    pkg-config installs correctly

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PkgConfig < Formula
   homepage 'http://sourceforge.net/projects/pkgconfiglite/'
-  url 'http://downloads.sourceforge.net/project/pkgconfiglite/0.28-1/pkg-config-lite-0.28-1.tar.gz'
-  sha256 '21b76ec4e115ee30f9b3077a2506e48e8b837332ed4d30c9776502e69c6a29e5'
+  url 'http://downloads.sourceforge.net/project/pkgconfiglite/0.27.1-1/pkg-config-lite-0.27.1-1.tar.gz'
+  sha256 '0fcac60ae7ad8e705c30fb5f8f83e75b09f54e1c80a2f6814c346bf5f1dba9a0'
 
   def install
     paths = %W[
