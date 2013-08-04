a3fc51ce1c96e893b9a651afcdbf659f6741bb68
Amos Wenger <amos@lockfree.ch>
2013-08-04 16:31:30 +0200

    Add comments to pkg-config formula

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class PkgConfig < Formula
+  # Actually using pkg-config-lite, which doesn't require glib
+  # 0.28.1-1 doesn't build (fails 4/25 tests on MinGW), stay with 0.27.x for now
   homepage 'http://sourceforge.net/projects/pkgconfiglite/'
   url 'http://downloads.sourceforge.net/project/pkgconfiglite/0.27.1-1/pkg-config-lite-0.27.1-1.tar.gz'
   sha256 '0fcac60ae7ad8e705c30fb5f8f83e75b09f54e1c80a2f6814c346bf5f1dba9a0'
