64f111ab55ef02c8fa2f92fb88d847451204b5d5
Amos Wenger <amos@lockfree.ch>
2013-08-04 03:46:41 +0200

    Use pkg-config-lite

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -1,16 +1,9 @@
 require 'formula'
 
 class PkgConfig < Formula
-  homepage 'http://pkgconfig.freedesktop.org'
-  url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz'
-  mirror 'http://fossies.org/unix/privat/pkg-config-0.28.tar.gz'
-  sha256 '6b6eb31c6ec4421174578652c7e141fdaae2dabad1021f420d8713206ac1f845'
-
-  bottle do
-    sha1 'fe503c105b952d04863931da96f03b0339d7fbb0' => :mountain_lion
-    sha1 'd483a4ed0eca160fb5c69d3a0a4011c3393ee2a3' => :lion
-    sha1 'e5859bdb284d4810057fd8cace202c60af40885f' => :snow_leopard
-  end
+  homepage 'http://sourceforge.net/projects/pkgconfiglite/'
+  url 'http://downloads.sourceforge.net/project/pkgconfiglite/0.28-1/pkg-config-lite-0.28-1.tar.gz'
+  sha256 '21b76ec4e115ee30f9b3077a2506e48e8b837332ed4d30c9776502e69c6a29e5'
 
   def install
     paths = %W[
@@ -24,7 +17,6 @@ class PkgConfig < Formula
         --disable-debug
         --prefix=#{prefix}
         --disable-host-tool
-        --with-internal-glib
         --with-pc-path=#{paths*':'}
       ]
     args << "CC=#{ENV.cc} #{ENV.cflags}" unless MacOS::CLT.installed?
