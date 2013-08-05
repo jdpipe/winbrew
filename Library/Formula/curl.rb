287e5062ad9db8c88505240a482d7c46f34034ac
Amos Wenger <amos@lockfree.ch>
2013-08-05 23:33:42 +0200

    Make curl compile. Yay!

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -6,34 +6,45 @@ class Curl < Formula
   mirror 'ftp://ftp.sunet.se/pub/www/utilities/curl/curl-7.31.0.tar.gz'
   sha256 '2c3a65eecca799f57f57163d40644ba642426230274a2dc51d0a6d049a63616a'
 
-  keg_only :provided_by_osx,
-            "The libcurl provided by Leopard is too old for CouchDB to use."
-
   option 'with-ssh', 'Build with scp and sftp support'
-  option 'with-ares', 'Build with C-Ares async DNS support'
-  option 'with-ssl', 'Build with Homebrew OpenSSL instead of the system version'
-  option 'with-darwinssl', 'Build with Secure Transport for SSL support'
+  option 'with-sspi', 'Build with SSPI support'
 
   depends_on 'pkg-config' => :build
-  depends_on 'libmetalink' => :optional
+
+  # There is on built-in OpenSSL on Windows
+  depends_on 'openssl'
+
+  # There is on built-in zlib on Windows
+  depends_on 'zlib'
+
   depends_on 'libssh2' if build.with? 'ssh'
-  depends_on 'c-ares' if build.with? 'ares'
-  depends_on 'openssl' if build.with? 'ssl'
+
+  # -Os causes the build to fail, sizeof(long) is undefined
+  # superenv doesn't yet support changing the optimization level
+  env :std
 
   def install
+    # Disable optimization the hard way
+    # With -Os (homebrew default), the sizeof(long) test fails
+    # With -O2 (automake default), the sizeof(size_t) test fails
+    ENV['CFLAGS'] = '-g -O0'
+    ENV['CXXFLAGS'] = '-g -O0'
+
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
+      --with-zlib=#{HOMEBREW_PREFIX}
+      --with-ssl=#{HOMEBREW_PREFIX}
     ]
 
-    args << "--with-libssh2" if build.with? 'ssh'
-    args << "--with-libmetalink" if build.with? 'libmetalink'
-    args << "--enable-ares=#{Formula.factory("c-ares").opt_prefix}" if build.with? 'ares'
-    args << "--with-ssl=#{Formula.factory("openssl").opt_prefix}" if build.with? 'ssl'
-    args << "--with-darwinssl" if build.with? 'darwinssl'
+    # Shared works with GCC 4.6 but not from GCC 4.7
+    # link: http://curl.haxx.se/mail/lib-2013-05/0129.html
+    args << '--disable-shared'
 
-    system "./configure", *args
-    system "make install"
+    args << '--with-libssh2=#{HOMEBREW_PREFIX}' if build.with? 'ssh'
+
+    system './configure', *args
+    system 'make', 'install'
   end
 end
