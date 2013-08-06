e968d3ec80f1410ddfc7641009b8f6e9a34a48f4
Amos Wenger <amos@lockfree.ch>
2013-08-06 17:02:02 +0200

    msys-man: depends on groff

--- Library/Formula/msys-man.rb
+++ Library/Formula/msys-man.rb
@@ -6,6 +6,8 @@ class MsysMan < Formula
   version '0.1.0'
   sha1 '5adcfd2483f4e2032de24d81e7ccf13ac6e1aa72'
 
+  depends_on 'groff'
+
   def install
     ENV['PREFIX'] = prefix
     system "make", "install" # if this fails, try separate make/make install steps
