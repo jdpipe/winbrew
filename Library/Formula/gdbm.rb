a2864d692f805973a6f7b3063a0b98265465b653
Amos Wenger <amos@lockfree.ch>
2013-08-06 20:06:50 +0200

    gdbm: patch the hell out of it to make it build on MinGW.

--- Library/Formula/gdbm.rb
+++ Library/Formula/gdbm.rb
@@ -10,10 +10,26 @@ class Gdbm < Formula
 
   def install
     ENV.universal_binary if build.universal?
+
+    # configure tests fail otherwise on GCC 4.7.x
+    ENV.O1
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--infodir=#{info}"
     system "make install"
+  end
+
+  def patches
+    # Various patches for MinGW compatibility:
+    # emulate fsync, don't use SIGPIPE, don't use ioctl,
+    # use winsock instead of inet.h
+    [
+      'https://gist.github.com/nddrylliog/6166764/raw/d480df7957269862a56fde8f54377b2b8e2291e8/fsync-emulation.patch',
+      'https://gist.github.com/nddrylliog/6166764/raw/8090b2ea762ebc5429ecbf961aeea01dbd50500f/testgdbm.patch',
+      'https://gist.github.com/nddrylliog/6166764/raw/c66cc60160568d179d8ed1efc279e621c8619774/winsock-configure.patch',
+      'https://gist.github.com/nddrylliog/6166764/raw/32e0cad4c5a35e7a7be97b0fb26cd7de4f1a2433/winsock.patch'
+    ]
   end
 end
