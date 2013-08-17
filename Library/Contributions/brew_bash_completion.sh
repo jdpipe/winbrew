134a46b89dde98ca65956c83917bf10791dac3c1
Amos Wenger <amos@lockfree.ch>
2013-08-17 19:04:43 +0200

    Add --mingw to brew search completion

--- Library/Contributions/brew_bash_completion.sh
+++ Library/Contributions/brew_bash_completion.sh
@@ -318,7 +318,7 @@ _brew_search ()
     local cur="${COMP_WORDS[COMP_CWORD]}"
     case "$cur" in
     --*)
-        __brewcomp "--debian --fedora --fink --macports --opensuse --ubuntu"
+        __brewcomp "--debian --fedora --fink --macports --mingw --opensuse --ubuntu"
         return
         ;;
     esac
